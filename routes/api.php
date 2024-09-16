690670c1a336dcb4bfecf8ac99e46575edcd048f
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-09-17 00:25:53 +0600

    Route prefix

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1', 'prefix' => 'v1',  'as'=>'', 'middleware' => ['auth:sanctum']], function () { 
+Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1', 'prefix' => 'v1',  'as'=>'v1.', 'middleware' => ['auth:sanctum']], function () { 
   Route::get('/projects/search/{title}', 'ProjectController@search')->name('projects.search'); 
   Route::apiResource('projects', 'ProjectController'); 
 });
 
-Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1\Admin', 'prefix' => 'v1/admin',  'as'=>'admin.', 'middleware' => ['auth:api']], function () { 
+Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1\Admin', 'prefix' => 'v1/admin',  'as'=>'v1.admin.', 'middleware' => ['auth:api']], function () { 
   Route::get('/projects/search/{title}', 'ProjectController@search')->name('projects.search'); 
   Route::apiResource('projects', 'ProjectController'); 
 });
